d58099406105bcb71925336eb4b67ecc1d2bf62e
Emmanuel Garcia <emmanuel.garcia.gtz@gmail.com>
2019-06-04 13:22:26 -0500

    respado de db

--- api/db_rest_1.sql
+++ api/db_rest_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysqlserver:3306
--- Generation Time: Jun 04, 2019 at 06:01 PM
+-- Generation Time: Jun 04, 2019 at 06:21 PM
 -- Server version: 8.0.16
 -- PHP Version: 7.2.2
 
@@ -21,6 +21,47 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_rest`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `package_type`
+--
+
+CREATE TABLE `package_type` (
+  `id_package_type` smallint(6) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `english` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `package_type`
+--
+
+INSERT INTO `package_type` (`id_package_type`, `name`, `english`) VALUES
+(1, 'Bolsa', 'Bag'),
+(2, 'Caja', 'Box'),
+(3, 'Botella', 'Bottle'),
+(4, 'Pieza', 'Piece');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id_product` int(11) NOT NULL,
+  `id_package_type` smallint(11) NOT NULL,
+  `id_unit` smallint(11) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `name` text NOT NULL,
+  `quantity_package` varchar(50) NOT NULL,
+  `price` double(11,2) NOT NULL,
+  `image_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -47,6 +88,20 @@ INSERT INTO `unit` (`id_unit`, `name`, `abbreviation`) VALUES
 --
 
 --
+-- Indexes for table `package_type`
+--
+ALTER TABLE `package_type`
+  ADD PRIMARY KEY (`id_package_type`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_package_type` (`id_package_type`,`id_unit`),
+  ADD KEY `id_unit` (`id_unit`);
+
+--
 -- Indexes for table `unit`
 --
 ALTER TABLE `unit`
@@ -57,10 +112,33 @@ ALTER TABLE `unit`
 --
 
 --
+-- AUTO_INCREMENT for table `package_type`
+--
+ALTER TABLE `package_type`
+  MODIFY `id_package_type` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `unit`
 --
 ALTER TABLE `unit`
   MODIFY `id_unit` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_unit`) REFERENCES `unit` (`id_unit`),
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`id_package_type`) REFERENCES `package_type` (`id_package_type`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
